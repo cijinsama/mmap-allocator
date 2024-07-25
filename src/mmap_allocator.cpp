6c7ef6a57cc38dbeb48a33d3519690f41b75e854
cijin <201870262@smail.nju.edu.cn>
2024-07-25 15:35:19 +0800

    fix: fix bugs

--- src/mmap_allocator.cpp
+++ src/mmap_allocator.cpp
@@ -1,5 +1,7 @@
 #include <mmap_allocator.h>
+namespace mm{
 void SetDefault(std::string filename) {
   default_allocator = static_cast<void *>(Allocator<char>::New(filename));
 }
 void FreeDefault() { delete static_cast<Allocator<char> *>(default_allocator); }
+}
